838da4dd5c4d8829cda9b75816f088b94c9c4f24
gegogabriel <gegogabriel@gmail.com>
2018-01-01 17:50:00 +0200

    T1: Created welcome page and added the Marvel base colors.

--- app/src/main/java/com/uid/team5/project/MainActivity.java
+++ app/src/main/java/com/uid/team5/project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.uid.team5.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.uid.team5.project.auth.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Intent intent = new Intent(this, LoginActivity.class);
+       // startActivity(intent);
     }
 }
