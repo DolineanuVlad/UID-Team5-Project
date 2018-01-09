5bbcd4f290be9afcb983ac653a7d7472ff1b7de5
Kari Tamas <kari_tomi@yahoo.com>
2018-01-09 21:27:53 +0200

    Task3 - created Budget list + adapter + service Task9 - created fragment+list

---
+++ app/src/main/java/com/uid/team5/project/adapters/ExpenseCategoryAdapter.java
@@ -0,0 +1,58 @@
+package com.uid.team5.project.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.uid.team5.project.R;
+import com.uid.team5.project.models.ExpenseCategory;
+
+import java.util.List;
+
+/**
+ * Created by Tamas on 1/9/2018.
+ */
+
+public class ExpenseCategoryAdapter extends BaseAdapter {
+    private List<ExpenseCategory> expenses;
+    private Context context;
+
+    public ExpenseCategoryAdapter(List<ExpenseCategory> expenses, Context context) {
+        this.expenses = expenses;
+        this.context = context;
+    }
+
+
+    @Override
+    public int getCount() {
+        return expenses.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return expenses.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View myRow = (view == null) ? inflater.inflate(R.layout.expense_list_item,viewGroup,false) : view;
+
+        TextView expenseName= (TextView) myRow.findViewById(R.id.expenseName);
+        TextView expenseDescription = (TextView) myRow.findViewById(R.id.expenseDescription);
+
+
+        expenseName.setText(expenses.get(position).getName());
+        expenseDescription.setText(expenses.get(position).getDescription());
+        return myRow;
+    }
+}
+
---
+++ app/src/main/java/com/uid/team5/project/services/ExpenseCategoryService.java
@@ -0,0 +1,28 @@
+package com.uid.team5.project.services;
+
+import com.uid.team5.project.models.ExpenseCategory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import wishlist.Goal;
+
+/**
+ * Created by Tamas on 1/9/2018.
+ */
+
+public class ExpenseCategoryService {
+        public static List<ExpenseCategory> getExpenses(){
+            List<ExpenseCategory> expenses=new ArrayList<ExpenseCategory>();
+
+            ExpenseCategory e1=new ExpenseCategory("food", "groceries+fruits");
+            ExpenseCategory e2=new ExpenseCategory("fun", "theathre+cinema+sports");
+
+            expenses.add(e1);
+            expenses.add(e2);
+
+            return expenses;
+        }
+}
+
+
