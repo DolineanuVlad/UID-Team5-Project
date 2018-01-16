4aa8f5d466becc0cccbfa3c47efa0269ba9d45b6
gegogabriel <gegogabriel@gmail.com>
2018-01-16 02:19:42 +0200

    Fix merge problem

--- app/src/main/java/com/uid/team5/project/adapters/AddExpensesAdapter.java
+++ app/src/main/java/com/uid/team5/project/adapters/AddExpensesAdapter.java
@@ -70,7 +70,7 @@ public class AddExpensesAdapter extends BaseAdapter {
             category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                 @Override
                 public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-                    expenses.get(position).setCategoryPosition(pos);
+                    expenses.get(position).setCategoryExpenseId(pos);
                     String[] categories = parent.getResources().getStringArray(R.array.category_arrays);
                     expenses.get(position).setCategory(categories[pos]);
                     notifyDataSetChanged();
